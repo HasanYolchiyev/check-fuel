54c7cc29230d4d6dc709c05baa4019d398a49506
HasanYolchiyev <frontendhy2008@gmail.com>
2023-08-05 11:21:38 +0400

    initial commit

---
+++ in-car/assets/js/index.js
@@ -0,0 +1,7 @@
+const km = prompt("nece km(yol):");
+const litr = prompt("nece litr(masinin benzini): ");
+let a=100;
+let b=a/km;
+let c=b*litr;
+console.log("100 km bu geder yandirar");
+console.log(c+"litr");
